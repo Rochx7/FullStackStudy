dbfb4b9d4ab0e26358066d135fa52c54cd2a79f9
Guilherme Rocha <67766327+Rochx7@users.noreply.github.com>
2020-11-22 16:38:59 -0300

    Primeira Landing Page

---
+++ FirstLandingPage/js/function.js
@@ -0,0 +1,19 @@
+$(window).on('load',function(){
+alert(" - PLUS ULTRA! - ");
+	}) 
+
+/*$(function(){
+	$('.artigo01').css('background-color', 'pink').css('text-align','center');
+}); */
+
+/*$(function(){
+	
+	setTimeout(function(){
+		$('#teste.artigo01').css('background-color', 'green');
+	},2000);
+}); */
+
+/*$(function(){
+	$('.artigo01 > p').css('color','red');
+		console.log($('.artigo01 > p').css('color'));
+	}); */
